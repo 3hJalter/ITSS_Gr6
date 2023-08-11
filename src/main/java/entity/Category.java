85d638662fe824a0a7cf65abdf6ec4bcfcb2fa3e
hienbui02 <hienbuiduy6@gmail.com>
2023-08-11 12:36:22 +0700

    modify category

--- src/main/java/entity/Category.java
+++ src/main/java/entity/Category.java
@@ -15,3 +15,24 @@ public class Category {
     private Double price;
     private Float priceMultiple;
 }
+
+public class General {
+    public static JSONArray convertResultSetToJsonArray(ResultSet resultSet) throws SQLException, JSONException {
+        JSONArray jsonArray = new JSONArray();
+
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        int columnCount = metaData.getColumnCount();
+
+        while (resultSet.next()) {
+            JSONObject jsonObject = new JSONObject();
+            for (int i = 1; i <= columnCount; i++) {
+                String columnName = metaData.getColumnName(i);
+                Object value = resultSet.getObject(i);
+                jsonObject.put(columnName, value);
+            }
+            jsonArray.put(jsonObject);
+        }
+
+        return jsonArray;
+    }
+}
