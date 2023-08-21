b8796d9d2c8c8b6c6e9a99228e22b8864695ca4d
Hà Huy Hoàng <101927338+3hJalter@users.noreply.github.com>
2023-08-21 19:34:22 +0700

    New JavaDocs

--- src/main/java/utils/SyntaxChecker.java
+++ src/main/java/utils/SyntaxChecker.java
@@ -11,7 +11,17 @@ import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * A utility class for syntax validation and checking in various contexts.
+ */
 public class SyntaxChecker {
+
+    /**
+     * Checks whether the given string is a valid integer ID.
+     *
+     * @param id The string to be checked.
+     * @return A response message indicating whether the ID is valid or not.
+     */
     public static ResponseMessage isId(String id) {
         if (id == null) return SyntaxResponseMessage.INVALID_ID_INPUT;
         try {
@@ -21,6 +31,15 @@ public class SyntaxChecker {
             return SyntaxResponseMessage.INVALID_ID_INPUT;
         }
     }
+
+    /**
+     * Parses and validates an ID parameter from the HTTP exchange.
+     *
+     * @param exchange      The HTTP exchange containing the request.
+     * @param parameterName The name of the parameter to be parsed and checked.
+     * @return The parsed and validated ID parameter value.
+     * @throws IOException If an I/O error occurs during parsing or response handling.
+     */
     public static String parseAndCheckIdParameter(HttpExchange exchange, String parameterName) throws IOException {
         String parameterValue = ControlAPI.parseQueryString(exchange.getRequestURI().getQuery(), parameterName);
         ResponseMessage response = SyntaxChecker.isId(parameterValue);
@@ -28,6 +47,12 @@ public class SyntaxChecker {
         return parameterValue;
     }
 
+    /**
+     * Checks whether the given string is a valid month.
+     *
+     * @param monthStr The string representing the month to be checked.
+     * @return A response message indicating whether the month is valid or not.
+     */
     public static ResponseMessage isMonth(String monthStr) {
         if (monthStr == null) return SyntaxResponseMessage.INVALID_MONTH_INPUT;
         int month;
@@ -40,6 +65,12 @@ public class SyntaxChecker {
         return SyntaxResponseMessage.INVALID_MONTH_INPUT;
     }
 
+    /**
+     * Checks whether the given string is a valid year.
+     *
+     * @param yearStr The string representing the year to be checked.
+     * @return A response message indicating whether the year is valid or not.
+     */
     public static ResponseMessage isYear(String yearStr) {
         if (yearStr == null) return SyntaxResponseMessage.INVALID_YEAR_INPUT;
         int year;
@@ -52,6 +83,12 @@ public class SyntaxChecker {
         return SyntaxResponseMessage.INVALID_YEAR_INPUT;
     }
 
+    /**
+     * Checks whether the given string is a valid UUID.
+     *
+     * @param uuid The string to be checked as a UUID.
+     * @return A response message indicating whether the UUID is valid or not.
+     */
     public static ResponseMessage isUUID(String uuid) {
         if (uuid == null) return SyntaxResponseMessage.INVALID_BARCODE_UUID;
         try {
@@ -61,6 +98,15 @@ public class SyntaxChecker {
             return SyntaxResponseMessage.INVALID_BARCODE_UUID;
         }
     }
+
+    /**
+     * Parses and validates a UUID parameter from the HTTP exchange.
+     *
+     * @param exchange      The HTTP exchange containing the request.
+     * @param parameterName The name of the parameter to be parsed and checked.
+     * @return The parsed and validated UUID parameter value.
+     * @throws IOException If an I/O error occurs during parsing or response handling.
+     */
     public static String parseAndCheckUUIDParameter(HttpExchange exchange, String parameterName) throws IOException {
         String parameterValue = ControlAPI.parseQueryString(exchange.getRequestURI().getQuery(), parameterName);
         ResponseMessage response = SyntaxChecker.isUUID(parameterValue);
@@ -68,6 +114,12 @@ public class SyntaxChecker {
         return parameterValue;
     }
 
+    /**
+     * Checks whether the given string is a valid 16-digit card number.
+     *
+     * @param number The string representing the card number to be checked.
+     * @return A response message indicating whether the card number is valid or not.
+     */
     public static ResponseMessage isCardNumber(String number) {
         if (number == null) return SyntaxResponseMessage.INVALID_CARD_NUMBER;
         Pattern pattern = Pattern.compile("^\\d{16}$");
@@ -76,6 +128,12 @@ public class SyntaxChecker {
                 : SyntaxResponseMessage.INVALID_CARD_NUMBER;
     }
 
+    /**
+     * Checks whether the given string is a valid name.
+     *
+     * @param name The string representing the name to be checked.
+     * @return A response message indicating whether the name is valid or not.
+     */
     public static ResponseMessage isValidName(String name) {
         // Check if the name starts with a non-space character
         if (name == null || name.isEmpty() || Character.isWhitespace(name.charAt(0))) {
@@ -88,6 +146,12 @@ public class SyntaxChecker {
                 : SyntaxResponseMessage.INVALID_NAME;
     }
 
+    /**
+     * Checks whether the given string is a valid 3-digit security code.
+     *
+     * @param code The string representing the security code to be checked.
+     * @return A response message indicating whether the security code is valid or not.
+     */
     public static ResponseMessage isValidSecurityCode(String code) {
         if (code == null) return SyntaxResponseMessage.INVALID_SECURITY_CODE;
         Pattern pattern = Pattern.compile("^\\d{3}$");
@@ -97,6 +161,15 @@ public class SyntaxChecker {
     }
 
 
+    /**
+     * Internal method to handle syntax validation responses and send appropriate responses.
+     *
+     * @param exchange      The HTTP exchange for responding to the request.
+     * @param parameterName The name of the parameter being checked.
+     * @param response      The response message indicating syntax validation result.
+     * @param returnResponse The response message to be returned when validation fails.
+     * @throws IOException If an I/O error occurs during response handling.
+     */
     private static void CheckSyntaxResponse(HttpExchange exchange, String parameterName, ResponseMessage response, ResponseMessage returnResponse) throws IOException {
         if (response != SyntaxResponseMessage.SUCCESSFUL) {
             ControlAPI.sendResponse(exchange, JsonFunction.convertToJson(
